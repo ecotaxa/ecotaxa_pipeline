4cc9afc977b3d0ec4fe18df321e1fb47e47480f3
“Sebastien <sebastien.galvagno@imev-mer.fr>
2023-07-13 06:20:13 +0200

    merge ecotaxaDataFrame

--- ecotaxaDataframe.py
+++ ecotaxaDataframe.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 Header = [(str,str)]
 
+class ecotaxaDataframe: pass
+
 class ecotaxaDataframe():
 
     # header: Header = []
@@ -12,37 +14,45 @@ class ecotaxaDataframe():
     def __init__(self, filename=None):
         if filename:
             self.filename = filename
-            self.reac_csv(filename)
+            self.read_csv(filename)
         else: 
-            header: pd.DataFrame()
-            data : pd.DataFrame()
+            self.header: pd.DataFrame()
+            self.data : pd.DataFrame()
 
-    def reac_csv(self, filename):
+    def read_csv(self, filename):
         self.header = self.read_header(filename)
         self.data = self.read_data(filename)
     
-    def read_header(self,filename):
+    def read_header(self,filename) -> pd.DataFrame:
         """
         Only read the two header lines
         """
         header_rows = [0,1]
-        df = pd.read_csv(filename, skiprows = lambda x: x not in header_rows)
+        df = pd.read_csv(filename, skiprows = lambda x: x not in header_rows, sep="\t")
         return df
 
-    def read_data(self,filename):
+    def read_data(self,filename) -> pd.DataFrame:
         """
         Read the cvs data with the header, 
         the function remove the line of type because it change the columns type to text
         """
         header_rows = [1]
-        df = pd.read_csv(filename, skiprows = lambda x: x in header_rows)
+        df = pd.read_csv(filename, skiprows = lambda x: x in header_rows, sep="\t")
         return df
 
-    def save_cvs(self):
-        """
-        TO DO
-        """
-        pass
+    def concat(self) -> pd.DataFrame:
+        df = pd.concat([self.header, self.data], ignore_index=True, sort=False)
+        return df
+
+    # def save_cvs(self,*args,**kwargs):
+    def to_cvs(self,filename):
+        df = self.concat()
+        # df.to_csv( args , kwargs )
+        df.to_csv( filename , index=False )
+
+    def to_tsv(self,filename):
+        df = self.concat()
+        df.to_csv( filename , index=False , sep="\t")
 
     def add_columns(self,header:Header,data:list=None):
         """
@@ -56,4 +66,16 @@ class ecotaxaDataframe():
         """
         pass
 
+    #, how="inner", on=["object_id"]
+    def merge_with(self, ecodf:ecotaxaDataframe):
+        self.header = pd.merge(self.header, ecodf.header, how="inner", on=["object_id"])
+        self.data = pd.merge(self.data, ecodf.data, how="inner", on=["object_id"])
+
+def merge(left: ecotaxaDataframe, right: ecotaxaDataframe, *args, **kwargs) -> ecotaxaDataframe:
+
+    df = ecotaxaDataframe()
+    df.header = pd.merge(left.header, right.header, args, kwargs)
+    df.data = pd.merge(left.data, right.data, args, kwargs)
+
+    return df
 
